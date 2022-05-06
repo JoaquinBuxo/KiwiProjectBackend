7e486c442b58a43a427261d7f74c19f2b7664f52
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-05-06 20:27:44 +0200

    functional backend

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require('express');
 
-const indexRouter = require('./routes/index');
-
 const app = express();
 
 app.use(express.json());
 
-app.use('/', indexRouter);
+const keyPhone = [
+  " ",
+  ".,",
+  "abc",
+  "def",
+  "ghi",
+  "jkl",
+  "mno",
+  "pqrs",
+  "tuv",
+  "wxyz"
+];
+
+function wordCombinations(numProvided, currentIndex = 0) {
+  const currentKey = numProvided[currentIndex];
+  const possibleCharactersForKey = keyPhone[currentKey];
+
+  if (currentIndex === numProvided.length - 1) {
+    return possibleCharactersForKey.split("");
+  }
+
+  const currentIteration = [];
+
+  for (let i = 0; i < possibleCharactersForKey.length; i++) {
+    const thisRound = wordCombinations(numProvided, currentIndex + 1);
+    thisRound.forEach((char) => {
+      currentIteration.push(`${possibleCharactersForKey[i]}${char}`);
+    });
+  }
+
+  return currentIteration;
+}
+
+app.get('/t9/:number', (req, res) => {
+  const {number} = req.params;
+  return res.json(wordCombinations(number));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
