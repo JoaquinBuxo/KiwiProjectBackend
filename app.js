99ecb0074e98aca222de2e3f12ec0d8232c105ee
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-05-08 18:47:55 +0200

    adding cors

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const t9 = require('./t9logic');
 
+app.use(cors());
 app.use(express.json());
-
-const keyPhone = [
-  " ",
-  ".,",
-  "abc",
-  "def",
-  "ghi",
-  "jkl",
-  "mno",
-  "pqrs",
-  "tuv",
-  "wxyz"
-];
-
-function wordCombinations(numProvided, currentIndex = 0) {
-  const currentKey = numProvided[currentIndex];
-  const possibleCharactersForKey = keyPhone[currentKey];
-
-  if (currentIndex === numProvided.length - 1) {
-    return possibleCharactersForKey.split("");
-  }
-
-  const currentIteration = [];
-
-  for (let i = 0; i < possibleCharactersForKey.length; i++) {
-    const thisRound = wordCombinations(numProvided, currentIndex + 1);
-    thisRound.forEach((char) => {
-      currentIteration.push(`${possibleCharactersForKey[i]}${char}`);
-    });
-  }
-
-  return currentIteration;
-}
 
 app.get('/t9/:number', (req, res) => {
   const {number} = req.params;
-  return res.json(wordCombinations(number));
+  return res.json(t9.wordCombinations(number));
 });
 
 // catch 404 and forward to error handler
---
+++ t9logic.js
@@ -0,0 +1,36 @@
+const keyPhone = [
+    "",
+    "",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tuv",
+    "wxyz"
+];
+
+const wordCombinations = (numProvided, currentIndex = 0) => {
+    const currentKey = numProvided[currentIndex];
+    const possibleCharactersForKey = keyPhone[currentKey];
+
+    if (currentIndex === numProvided.length - 1) {
+        return possibleCharactersForKey.split("");
+    }
+
+    const currentIteration = [];
+
+    for (let i = 0; i < possibleCharactersForKey.length; i++) {
+        const thisRound = wordCombinations(numProvided, currentIndex + 1);
+        thisRound.forEach((char) => {
+            currentIteration.push(`${possibleCharactersForKey[i]}${char}`);
+        });
+    }
+
+    return currentIteration;
+}
+
+exports.wordCombinations = wordCombinations;
+
+
