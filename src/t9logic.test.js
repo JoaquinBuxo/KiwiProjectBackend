ca990faa5913776acb870df6faa79d00a64d1129
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-05-08 19:14:05 +0200

    adding unit tests

--- src/t9logic.test.js
+++ src/t9logic.test.js
@@ -1,3 +1,20 @@
+const { wordCombinations } = require("./t9logic");
+
 describe("t9logic", () => {
-  it.todo("test");
+  it("returns abc for number 2", () => {
+    const result = wordCombinations("2");
+    expect(result).toEqual(["a", "b", "c"]);
+  });
+
+  it("returns hola, but not kiwi for 4652", () => {
+    const result = wordCombinations("4652");
+    expect(result).toContain("hola");
+    expect(result).not.toContain("kiwi");
+  });
+
+  it("returns kiwi, but not hola for 5494", () => {
+    const result = wordCombinations("5494");
+    expect(result).toContain("kiwi");
+    expect(result).not.toContain("hola");
+  });
 });
